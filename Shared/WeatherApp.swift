91448857600ef5cfc1088981d851c8f224b7fea2
Jesse Dudgeon <jessedudgeon@gmail.com>
2022-07-22 19:08:09 -0400

    Initial Commit

---
+++ Shared/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Shared
+//
+//  Created by Jesse Dudgeon on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
